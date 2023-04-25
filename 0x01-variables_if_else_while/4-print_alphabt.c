f8e886586744c10c97f142f9049be3ac8783d1ce
Diana-R-M <128912281+Diana-R-M@users.noreply.github.com>
2023-04-25 04:53:01 +0300

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,13 @@
 
 int main(void)
 {
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	putchar(alphabet);
-
-if(alphabet != 'e' && alphabet != 'q')
-{
-	putchar(alphabet);
-}
-putchar('\n');
-return (0);
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	if(alphabet != 'e' && alphabet != 'q')
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
 }
