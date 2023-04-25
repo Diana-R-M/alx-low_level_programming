6feddd35ae537060a79b25ff94e8a149d3c2b1fd
Diana-R-M <128912281+Diana-R-M@users.noreply.github.com>
2023-04-25 04:58:07 +0300

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- *Main - Programs single digits of base 10 thats is  0 to 9
- *
- *Return: Always 0(Success)
+ * main - Entry point
+ * Description: 'single digits of base 10 thats is  0 to 9'
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 
 	int a;
-	for (a = 0; a < 10; a++)
-		printf("%d", a);
+for (a = 0; a < 10; a++)
+	printf("%d", a);
 
 	printf("\n");
-
-	return(0);
+	
+return(0);
 }
