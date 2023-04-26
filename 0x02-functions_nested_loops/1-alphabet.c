c0d8fa22646efe74dbb49842c663865cb0dad849
Diana <diana@example.com>
2023-04-26 15:46:11 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'prints the alphabet, in lowercase.'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'Checks if a random number is positive, zero or negative.'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int alphabet10x;
+
+	for (alphabet10x = 1; alphabet10x <= 10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+
+		putchar('\n');
+
+		return (0);
+	}
+}
