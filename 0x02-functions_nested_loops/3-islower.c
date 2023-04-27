c6f6fe58e00ca674562ea6b43f439c7b2ab685fe
Diana-R-M <128912281+Diana-R-M@users.noreply.github.com>
2023-04-27 05:50:21 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 /**
  * _islower - Entry point
  * Description: 'Returns 1 if c is a letter, lowercase or uppercase.'
+ *@c: Parameter to be checked
  * Return: Always 0 (Success)
  */
 int _islower(int c)
