e35923e5f8d285cc6952b65e4cdd7c234d74848c
Diana-R-M <128912281+Diana-R-M@users.noreply.github.com>
2023-04-27 05:44:36 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * Description: 'Returns 1 if c is a letter, lowercase or uppercase.'
  * Return: Always 0 (Success)
  */
-int _isalpha(int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
