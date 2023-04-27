9dcd711f9c0cbaa13826416e7496c992975bdf66
Diana-R-M <128912281+Diana-R-M@users.noreply.github.com>
2023-04-27 04:35:31 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet_x10 - Entry point
  * Description: 'Checks if a random number is positive, zero or negative.'
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 	char alphabet;
-	int alphabet10x;
+	int alphabet_10x;
 
-	for (alphabet10x = 1; alphabet10x <= 10; i++)
+	for (alphabet_10x = 1; alphabet_10x <= 10; i++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
 
 		_putchar('\n');
 
-		return (0);
 	}
 }
