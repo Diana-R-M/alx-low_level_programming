8402e8d94990de5f7c8d97881518682a83b11b3c
Diana-R-M <128912281+Diana-R-M@users.noreply.github.com>
2023-04-29 05:47:51 +0300

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ void print_most_numbers(void)
 {
 	int a = 0;
 
-	for(a = 0; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		if(a != 2 || a != 4)
+		if (a != 2 || a != 4)
 		_putchar((a % 10) + '0');
 	}
 		_putchar ('\n');
