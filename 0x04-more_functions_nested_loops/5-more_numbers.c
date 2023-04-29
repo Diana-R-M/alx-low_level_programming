f9628bf10485615e5897557f8fb9b0486be0bb4e
Diana <diana@example.com>
2023-04-28 19:42:52 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 {
 	int a,b;
 
-	for(a = '1'; a <= '10'; a++)
+	for(a = 1; a <= 10; a++)
 	{
-		for(b = '0'; b <= '14'; b++)
+		for(b = 0; b <= 14; b++)
 		{
 			if(b > 10)
 				_putchar((b/10) + '0');
