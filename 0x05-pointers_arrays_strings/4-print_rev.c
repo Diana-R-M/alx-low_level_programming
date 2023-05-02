9239f748b30bebcaccd5b93e2f4a982ddd354be7
Diana-R-M <128912281+Diana-R-M@users.noreply.github.com>
2023-05-02 23:59:07 +0300

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,23 +3,22 @@
 /**
  * print_rev - entry point
  * Description: 'prints a string, in reverse'
- * @str: string pointer
+ * @s: string pointer
  * Return: Always 0
  */
 
 void print_rev(char *s)
 {
 	int a = 0;
-	int b;
+	int b, c;
 
 	while (*s != '\0')
 	{
 		a++;
 		s++;
 	}
-	
-	s--;
-	for (b = a; b > 0; b--)
+	c = a;
+	for (b = a -1; b >= 0; b--)
 	{
 		_putchar(s[b]);
 	}
