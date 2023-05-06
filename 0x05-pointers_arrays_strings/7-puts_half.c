3fa60fb202d038e26e099c90186b84708e471c11
Diana-R-M <128912281+Diana-R-M@users.noreply.github.com>
2023-05-06 07:05:52 +0300

    Updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,6 @@
 /**
  * puts_half - entry point
  * Description: 'print the second half of the string'
- * n = (length_of_the_string - 1) / 2
  * @str: string pointer
  * Return: Always 0
  */
@@ -15,15 +14,15 @@ void puts_half(char *str)
 	a = 0;
 
 	for (c = 0; str[c] != '\0'; c++)
-		a++;
+		c++;
 
-	n = (a / 2);
+	b = (a / 2);
 
 	if ((a % 2) == 1)
-		n = ((a + 1) / 2);
+		b = ((a + 1) / 2);
 
-	for (c = n; str[c] != '\0'; c++)
-		_putchar(str[c]);
+	for (a = b; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
 }
 
