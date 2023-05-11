3af6036092cfcf3e49ff83dee98a64bf65f9807f
Diana <diana@example.com>
2023-05-11 11:48:11 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_prime_number - determines if int is prime or not
+ * @n: int value
+ * Return: 1 if n == prime number, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime_number(n, n - 1));
+}
+
+/**
+ * prime_number - recursive calculation of prime number
+ * @a: int value
+ * @b: iterator
+ * Return: 1 if a == prime, 0 if not
+ */
+int prime_number(int a, int b)
+{
+	if (a % b == 0 && b > 0)
+	{
+		return (0);
+	}
+	else if (b == 1)
+	{
+		return (1);
+	}
+	return (prime_number(a, b - 1));
+}
