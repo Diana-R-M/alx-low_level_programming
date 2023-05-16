acd996d188a93bf548f8724f5982068a72298e0c
Diana <diana@example.com>
2023-05-16 07:47:49 -0700

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates new string to new memory
+ * @str: char input
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	unsigned int b, c;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	b = 0;
+	while (str[b] != '\0')
+		b++;
+	c = 0;
+	a = malloc(sizeof(char) * (c + 1));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; str[c]; c++)
+		a[c] = str[c];
+	return (a);
+}
+
