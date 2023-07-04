3f945e8b2bf4c6a6663046b6f307514bbe40c673
Diana-R-M <diana@github.com>
2023-07-04 00:43:29 -0700

    Updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -17,12 +17,11 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (*head)
+	for (; *head; *head = node)
 	{
 		node = (*head)->next;
-		free(*head);
 
-		*head = node;
+		free(*head);
 	}
 
 	*head = NULL;
