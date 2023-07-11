30da1b611b08a014f754433aa59f4f78d89fa736
Diana-R-M <diana@github.com>
2023-07-10 19:29:07 -0700

    creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,18 +15,16 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file, written_bytes = 0;
-	int len;
+	ssize_t len = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	while (text_content != NULL)
+	while (text_content[len] != '\0')
 	{
-		for (len = 0; text_content[len];)
-
-			len++;
+		len++;
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
